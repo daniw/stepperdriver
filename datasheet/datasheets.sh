4abc21f821ba0d729e4a417d79970c0017777eba
daniw <dani.winz@bluewin.ch>
2014-06-08 20:50:59 +0200

    Datenblätter für einige Schrittmotortreiber

---
+++ datasheet/datasheets.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+### Motor driver ###
+
+# Stepper motor driiver
+wget -nc -O L6470.pdf http://www.st.com/web/en/resource/technical/document/datasheet/CD00255075.pdf
+wget -nc -O L6472.pdf http://www.st.com/web/en/resource/technical/document/datasheet/DM00047136.pdf
+wget -nc -O L6480.pdf http://www.st.com/web/en/resource/technical/document/datasheet/DM00056884.pdf
+wget -nc -O L6474.pdf http://www.st.com/web/en/resource/technical/document/datasheet/DM00043117.pdf
+wget -nc -O L9942.pdf http://www.st.com/web/en/resource/technical/document/datasheet/CD00072898.pdf
+wget -nc -O L6482.pdf http://www.st.com/st-web-ui/static/active/en/resource/technical/document/datasheet/DM00067131.pdf
+
